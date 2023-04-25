7ccc7272ec65ffc198950781dc5d90312ae874da
hassan hafez <hassanhafez86@gmail.com>
2023-04-25 04:09:09 +0200

    handle form post request, handle success and error messages

---
+++ client/script.js
@@ -0,0 +1,70 @@
+const imagesInput = document.getElementById("images");
+const selectedImagesList = document.getElementById("selected-images");
+const imagesTobase64 = [];
+const images = [];
+const addProductForm = document.querySelector("form");
+const error = document.getElementById("error");
+const success = document.getElementById("success");
+
+imagesInput.addEventListener("change", (event) => {
+  for (const file of event.target.files) {
+    const listItem = document.createElement("li");
+    listItem.textContent = file.name;
+    selectedImagesList.appendChild(listItem);
+    imagesTobase64.push(file);
+  }
+
+  const fileInput = document.querySelector('input[type="file"]');
+  const files = fileInput.files;
+
+  for (let i = 0; i < files.length; i++) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const base64String = event.target.result.split(",")[1];
+      images.push({ attachment: base64String });
+    };
+    reader.readAsDataURL(files[i]);
+  }
+});
+
+addProductForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    const endpoint = "http://localhost:3000/api/shopify/products";
+    const response = await fetch(endpoint, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        product: {
+          title: addProductForm.title.value,
+          body_html: addProductForm.description.value,
+          tags: addProductForm.tags.value,
+          images: images,
+          variants: [
+            {
+              price: addProductForm.price.value,
+              sku: addProductForm.sku.value,
+            },
+          ],
+        },
+      }),
+    });
+
+    if (response.ok) {
+      error.innerText = "";
+      success.style.display = "block";
+      success.innerText = "Product added successfully!";
+      event.target.reset();
+      selectedImagesList.innerText = "";
+    } else {
+      success.innerText = "";
+      error.style.display = "block";
+      error.innerText = `Error: product was not added to shopify due to technical error - ${response.status}`;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
