36d06403a43ef9b030b9f0f85c928232c65429ba
hassan hafez <hassanhafez86@gmail.com>
2023-04-25 07:42:15 +0200

    Add Procfile

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 9000;
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
-
-app.get("/", async (req, res) => res.send("Shopify App"));
+app.get(express.static("client"));
 
 app.post("/api/shopify/products", async (req, res) =>
   res.json(await addProduct(req.body, process.env.TOKEN))
